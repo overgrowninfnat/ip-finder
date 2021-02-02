58e130ea1373f5d19575cf871c3b76136d908fb8
Giuseppe Di Giorno <giuseppedigiorno@yahoo.it>
2021-02-02 19:35:23 +0100

    Mobile design done

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,17 @@ export default function Home() {
   }, [])
   return (
     <>
-    {Object.keys(apiData).length === 0 ? <div>Loading...</div> : (<div className='absolute h-full w-full'>
+    {Object.keys(apiData).length === 0 ? (
+      <div className="w-full h-screen flex flex-col justify-center items-center bg-gray-100">
+        <svg className="animate-spin h-12 w-12 text-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+          <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+          <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+        </svg>
+        <p className="text-black text-xl font-medium mt-2">We are retrieving your IP Address</p>
+      </div>
+    ) 
+    : 
+    (<div className='absolute h-full w-full'>
     {/* Bg Image Container */}
     <div className="fixed w-full h-1/3 -z-1">
       <Image
